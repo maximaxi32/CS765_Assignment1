b8cc25f752919be5fcaf7e8125d9931012e1200e
rishabhIITB <rishabh.singh260700@gmail.com>
2024-02-01 00:24:27 +0530

    added dfs code

--- Network.py
+++ Network.py
@@ -3,7 +3,20 @@ def createNetwork(ListofPeers):
     pass
 
 # To check if the network is a connected graph or not
-def isConnected(ListofPeers):
-    # TODO: Implement BFS/DFS/union find
-    pass
+
+
+def isConnected(ListOfPeers):
+    if not ListOfPeers:
+        return True
+    visited = set()
+    start_node = ListOfPeers[0]
+    dfs(start_node, visited)
+    return len(visited) == len(ListOfPeers)
+
+def dfs(node, visited):
+    visited.add(node)
+    currNeighbors = node.GetNeighbors()
+    for neighbor in currNeighbors:
+        if neighbor not in visited:
+            dfs(neighbor, visited)
 
